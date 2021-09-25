addc1fecab72c5e4ae23f09cb5503e1c9356660b
Mico Mallari <micoangelo.mallari.cics@ust.edu.ph>
2021-09-25 11:03:22 +0400

    image + logo spin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
+import logo from './logo.svg';          // imports from within the same folder because of './'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1> welcome page </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Edit <code>src/App.js</code> <em>and save to reload.</em> <b> Hi there</b>.
+        Edit <code>src/App.js</code> <em>and save to reload.</em> <b> brrrrrrrrrrr </b>.
         </p>
+        <img src="https://i.imgflip.com/3nm1gx.png" 
+        alt="yep"/>
         <a
           className="App-link"
           href="https://reactjs.org"
